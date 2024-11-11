17caa49af2f73df2b1549c66613ae793fd262167
Codetamer333 <kattalynho_no1@yahoo.com>
2024-11-11 02:33:48 +0200

    8

--- lovecoupons.js
+++ lovecoupons.js
@@ -12,7 +12,7 @@ const client = new Client({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT,           
+    port: process.env.DB_PORT,           
 });
 
 await client.connect(); // Conectare la baza de date
